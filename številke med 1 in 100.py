69c1f08b7dadf97339cb4a84e0e9c96c7a10ae54
rgornik <43386611+rgornik@users.noreply.github.com>
2018-10-07 19:25:01 +0200

    deljenje številke

---
+++ številke med 1 in 100.py
@@ -0,0 +1,17 @@
+print "Dobrodosli v igri fizzbuzz"
+
+izberi = raw_input("Izberi stevilko med 1 in 100: " )
+
+try:
+    izberi = int(izberi)
+    for izberi in range(1, izberi+1):
+        if izberi % 3 == 0:
+            print "fizz"
+        elif izberi % 5 == 0:
+            print "buzz"
+        elif izberi % 3 == 0 and izberi % 5 == 0:
+            print "fizzbuzz"
+        else:
+            print izberi
+except Exception as e:
+    print "Vnesi celo stevilo"
